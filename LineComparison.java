d3ac82353bdc825cc3f34e5cf38672b66e0e6347
Mayoor <mayoordoiphode@gmail.com>
2022-04-19 16:57:47 +0530

    Added: Welcome messege program

---
+++ LineComparison.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.assignment8.LineComp;
+
+/**
+ * @author mayoor
+ *
+ */
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line comaparison program..!");
+	}
+}
